da3cdf506ee54c2bd05b6c1ad7589b0672dc64a8
1ba1 <vvvalenti01@yahoo.it>
2019-08-06 12:31:41 +0200

    fix indentation

--- src/components/Book.js
+++ src/components/Book.js
@@ -31,31 +31,31 @@ const Book = ({
   },
   clickHandler,
 }) => (
-    <tr style={styles}>
-      <td style={{ padding: 20 }}>
-        <p>{category}</p>
-        <h3>{title}</h3>
-        <p>{author}</p>
-        <button
-          type="button"
-          style={removeButton}
-          onClick={() => clickHandler(id)}
-        >
-          Remove book
-        </button>
-      </td>
+  <tr style={styles}>
+    <td style={{ padding: 20 }}>
+      <p>{category}</p>
+      <h3>{title}</h3>
+      <p>{author}</p>
+      <button
+        type="button"
+        style={removeButton}
+        onClick={() => clickHandler(id)}
+      >
+        Remove book
+      </button>
+    </td>
 
-      <td>
-        64%
-      </td>
+    <td>
+      64%
+    </td>
 
-      <td style={{ padding: '0 20px' }}>
-        <p>Current chapter</p>
-        <p>Chapter Nth</p>
-        <button type="button" style={buttonStyles}>UPDATE PROGRESS</button>
-      </td>
-    </tr>
-  );
+    <td style={{ padding: '0 20px' }}>
+      <p>Current chapter</p>
+      <p>Chapter Nth</p>
+      <button type="button" style={buttonStyles}>UPDATE PROGRESS</button>
+    </td>
+  </tr>
+);
 
 Book.propTypes = {
   book: PropTypes.object.isRequired,
