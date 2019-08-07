5d4d9b115afd802c9bf8753c84bbd0d8aa779db7
1ba1 <vvvalenti01@yahoo.it>
2019-08-07 12:45:12 +0200

    add ChapterSelector

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import ChapterSelector from './ChapterSelector';
 import '../css/book.css';
 
 const styles = {
@@ -7,14 +8,6 @@ const styles = {
   borderRadius: 4,
   border: '1px solid #eee',
   backgroundColor: '#fff',
-};
-
-const buttonStyles = {
-  padding: '5px 10px',
-  backgroundColor: '#09f',
-  color: '#fff',
-  border: 'none',
-  borderRadius: 4,
 };
 
 const removeButton = {
@@ -25,57 +18,71 @@ const removeButton = {
   color: '#09f',
 };
 
-const Book = ({
-  book: {
-    title, author, category, id,
-  },
-  clickHandler,
-}) => (
-  <tr style={styles}>
-    <td style={{ padding: 15 }}>
-      <p>{category}</p>
-      <h3>{title}</h3>
-      <p>{author}</p>
-      <button
-        type="button"
-        style={removeButton}
-        onClick={() => clickHandler(id)}
-      >
-        Remove book
-      </button>
-    </td>
+class Book extends React.Component {
+  state = {
+    progress: 0,
+  }
 
-    <td className="middleCol">
-      <svg className="circular-chart" viewBox="0 0 36 36">
-        <path
-          x="50%"
-          y="50%"
-          className="circle-shadow"
-          stroke="100"
-          d="M18 2.0845
-            a 15.9155 15.9155 0 0 1 0 31.831
-            a 15.9155 15.9155 0 0 1 0 -31.831"
-        />
-        <path
-          x="50%"
-          y="50%"
-          className="circle"
-          strokeDasharray="64, 100"
-          d="M18 2.0845
-            a 15.9155 15.9155 0 0 1 0 31.831
-            a 15.9155 15.9155 0 0 1 0 -31.831"
-        />
-      </svg>
-      <p>64% completed</p>
-    </td>
+  updateProgress = (chapter) => {
+    this.setState({ progress: chapter * 5 });
+  };
 
-    <td style={{ padding: '0 15px' }}>
-      <p>Current chapter</p>
-      <p>Chapter Nth</p>
-      <button type="button" style={buttonStyles}>UPDATE PROGRESS</button>
-    </td>
-  </tr>
-);
+  render() {
+    const {
+      book: {
+        title, author, category, id,
+      },
+      clickHandler,
+    } = this.props;
+
+    const { progress } = this.state;
+
+    return (
+      <tr style={styles}>
+        <td style={{ padding: 15 }}>
+          <p>{category}</p>
+          <h3>{title}</h3>
+          <p>{author}</p>
+          <button
+            type="button"
+            style={removeButton}
+            onClick={() => clickHandler(id)}
+          >
+            Remove book
+          </button>
+        </td>
+
+        <td className="middleCol">
+          <svg className="circular-chart" viewBox="0 0 36 36">
+            <path
+              x="50%"
+              y="50%"
+              className="circle-shadow"
+              stroke="100"
+              d="M18 2.0845
+                a 15.9155 15.9155 0 0 1 0 31.831
+                a 15.9155 15.9155 0 0 1 0 -31.831"
+            />
+            <path
+              x="50%"
+              y="50%"
+              className="circle"
+              strokeDasharray={`${progress}, 100`}
+              d="M18 2.0845
+                a 15.9155 15.9155 0 0 1 0 31.831
+                a 15.9155 15.9155 0 0 1 0 -31.831"
+            />
+          </svg>
+          <p>{`${progress}% completed`}</p>
+        </td>
+
+        <td style={{ padding: '0 15px' }}>
+          <ChapterSelector updateHandler={this.updateProgress} />
+        </td>
+      </tr>
+    );
+  }
+}
 
 Book.propTypes = {
   book: PropTypes.object.isRequired,
---
+++ src/components/ChapterSelector.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const buttonStyles = {
+  padding: '5px 10px',
+  backgroundColor: '#09f',
+  color: '#fff',
+  border: 'none',
+  borderRadius: 4,
+  marginTop: 10,
+  cursor: 'pointer',
+};
+
+let chapters = [...Array(20).keys()].map(x => x + 1);
+
+chapters = chapters.map(num => (
+  <option key={num}>{`Chapter ${num}`}</option>
+));
+
+class ChapterSelector extends React.Component {
+  state = {
+    chapter: null,
+  }
+
+  handleChange = (e) => {
+    const chapter = e.target.value.split(' ')[1];
+    this.setState({ chapter: +chapter });
+  };
+
+  render() {
+    const { chapter } = this.state;
+    const { updateHandler } = this.props;
+    return (
+      <div style={{ textAlign: 'center' }}>
+        <p>Current chapter</p>
+        <select
+          value={`Chapter ${chapter}`}
+          onChange={this.handleChange}
+        >
+          {chapters}
+        </select>
+        <br />
+        <button
+          type="button"
+          style={buttonStyles}
+          onClick={() => updateHandler(chapter)}
+        >
+          UPDATE PROGRESS
+        </button>
+      </div>
+    );
+  }
+}
+
+ChapterSelector.propTypes = {
+  updateHandler: PropTypes.func.isRequired,
+};
+
+export default ChapterSelector;
